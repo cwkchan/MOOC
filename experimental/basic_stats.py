c7f1c2faf7d8aa24048276705731a624089f5cb9
Eric Koo <erickoo@umich.edu>
2014-04-16 22:31:51 -0400

    Added generate_plot functions for course_grades

--- experimental/basic_stats.py
+++ experimental/basic_stats.py
@@ -30,6 +30,118 @@ args = parser.parse_args()
 
 conn = get_connection()
 
+def generate_plot_percentage_range(bins):
+    try:
+        query = """SELECT MIN(normal_grade) AS accomplishment_grade
+                   FROM   course_grades
+                   WHERE  achievement_level='normal'"""
+        accomplishment_grade = pd.io.sql.read_frame(query, schemaconn.raw_connection()).ix[0,0]
+
+        query = """SELECT MAX(temp.lectures_viewed) AS lecture_total
+                   FROM   (
+                          SELECT COUNT(DISTINCT(item_id)) AS lectures_viewed
+                          FROM   lecture_submission_metadata
+                          GROUP BY session_user_id
+                          ) temp;"""
+        lecture_total = pd.io.sql.read_frame(query, schemaconn.raw_connection()).ix[0,0]
+
+        query = """SELECT course_grades.session_user_id
+                        , course_grades.normal_grade
+                        , lecture_submissions.lectures_viewed
+                   FROM   course_grades
+                          LEFT JOIN (
+                               SELECT session_user_id
+                                     , 100*COUNT(DISTINCT(item_id))/%s AS lectures_viewed
+                               FROM   lecture_submission_metadata
+                               GROUP BY session_user_id) lecture_submissions
+                          ON course_grades.session_user_id=lecture_submissions.session_user_id
+                   WHERE course_grades.normal_grade > 0;""" % lecture_total
+        lectures_viewed = pd.io.sql.read_frame(query, schemaconn.raw_connection())
+
+        grades_by_lectures_viewed = []
+        labels = []
+        for bin in bins:
+            grades_by_lectures_viewed.append( lectures_viewed.loc[(lectures_viewed['lectures_viewed']>bin[0]) & (lectures_viewed['lectures_viewed']<=bin[1]),'normal_grade'].values )
+            labels.append(str(bin[0])+'-'+str(bin[1])+'%')
+
+       # Plot course grade distribution, colored by percent of lectures viewed
+        colors = []
+        bin_num = len(bins)
+        for i in range(bin_num):
+            colors.append((float(i)/bin_num,0.2,1-float(i)/bin_num))
+        n, bins, patches = plt.hist(grades_by_lectures_viewed, 100, histtype='bar', stacked=True, color=colors, label=labels)
+        plt.axvline(x=accomplishment_grade, color='#999999')
+        plt.legend(title='% of lectures accessed')
+        plt.title('Distribution of Course Grades ('+str(schema_name)+')')
+        plt.xlabel('Grade')
+        plt.ylabel('Count')
+
+    except Exception, e:
+        print e
+
+def generate_plot_week_range(bins):
+    try:
+        query = 'SELECT session_id,start,duration FROM coursera_index WHERE session_id="%s"' % schema_name
+        coursera_index = pd.io.sql.read_frame(query, conn.raw_connection(), index_col='session_id')
+        start_timestamp = int(time.mktime(time.strptime(coursera_index.loc[schema_name,'start'], '%m/%d/%Y'))) - time.timezone
+        duration = coursera_index.loc[schema_name,'duration']
+        timestamp_bins = []
+        labels = []
+        for bin in bins:
+            if bin[1]!=None and bin[1]<=duration:
+                timestamp_bins.append(((bin[0]-1)*604800+start_timestamp,bin[1]*604800+start_timestamp))
+                if bin[0]==bin[1]:
+                    labels.append('Week '+str(bin[1]))
+                else:
+                    labels.append('Week '+str(bin[0])+'-'+str(bin[1]))
+            else:
+                timestamp_bins.append(((bin[0]-1)*604800+start_timestamp,None))
+                if bin[0]<duration:
+                    labels.append('Week '+str(bin[0])+'-Beyond')
+                else:
+                    labels.append('Beyond')
+                break
+
+        query = """SELECT MIN(normal_grade) AS accomplishment_grade
+                   FROM   course_grades
+                   WHERE  achievement_level='normal'"""
+        accomplishment_grade = pd.io.sql.read_frame(query, schemaconn.raw_connection()).ix[0,0]
+
+        query = """SELECT course_grades.session_user_id
+                        , normal_grade
+                        , last_submission_time
+                   FROM   course_grades
+                          LEFT JOIN (
+                               SELECT session_user_id
+                                    , MAX(submission_time) AS last_submission_time
+                               FROM   lecture_submission_metadata
+                               GROUP BY session_user_id) lecture_submissions
+                          ON course_grades.session_user_id=lecture_submissions.session_user_id
+                   WHERE normal_grade>0;"""
+        last_lecture_views = pd.io.sql.read_frame(query, schemaconn.raw_connection())
+
+        grades_by_last_lecture_view = []
+        for bin in timestamp_bins:
+            if bin[1]!=None:
+                grades_by_last_lecture_view.append( last_lecture_views.loc[(last_lecture_views['last_submission_time']>bin[0]) & (last_lecture_views['last_submission_time']<=bin[1]),'normal_grade'].values )
+            else:
+                grades_by_last_lecture_view.append( last_lecture_views.loc[(last_lecture_views['last_submission_time']>bin[0]),'normal_grade'].values )
+
+       # Plot course grade distribution, colored by last lecture view
+        colors = []
+        bin_num = len(timestamp_bins)
+        for i in range(bin_num):
+            colors.append((float(i)/bin_num,0.2,1-float(i)/bin_num))
+        n, bins, patches = plt.hist(grades_by_last_lecture_view, 100, histtype='bar', stacked=True, color=colors, label=labels)
+        plt.axvline(x=accomplishment_grade, color='#999999')
+        plt.legend(title='Last lecture view')
+        plt.title('Distribution of Course Grades ('+str(schema_name)+')')
+        plt.xlabel('Grade')
+        plt.ylabel('Count')
+
+    except Exception, e:
+        print e
+
 if (args.schemas != None):
     schemas = args.schemas.split(",")
     query = """SELECT session_id,course,start,duration FROM coursera_index WHERE """
@@ -83,7 +195,7 @@ for schema_name in schemas:
         print '  '+str(int(100*1.0*statement_of_accomplishment/active))+'% of active users earned a Statement of Accomplishment ('+str(statement_of_accomplishment)+' total)'
         print ''
     except Exception, e:
-        pass
+        print e
 
     # Lecture submissions
     try:
@@ -132,61 +244,13 @@ for schema_name in schemas:
         pass
 
     # Course grades
+    plt.figure()
+    plt.subplot(2, 1, 1)
+    generate_plot_percentage_range([(0,10),(10,20),(20,30),(30,40),(40,50),(50,60),(60,70),(70,80),(80,90),(90,100)])
+    plt.subplot(2, 1, 2)
+    generate_plot_week_range([(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10),(11,11),(12,12),(13,13),(14,14),(15,15),(16,16),(17,17),(18,18),(19,19),(20,20)])
+
     try:
-        query = """SELECT MIN(normal_grade) AS accomplishment_grade
-                   FROM   course_grades
-                   WHERE  achievement_level='normal'"""
-        accomplishment_grade = pd.io.sql.read_frame(query, schemaconn.raw_connection()).ix[0,0]
-
-        query = """SELECT MAX(temp.lectures_viewed) AS lecture_total
-                   FROM   (
-                          SELECT COUNT(DISTINCT(item_id)) AS lectures_viewed
-                          FROM   lecture_submission_metadata
-                          GROUP BY session_user_id
-                          ) temp;"""
-        lecture_total = pd.io.sql.read_frame(query, schemaconn.raw_connection()).ix[0,0]
-
-        query = """SELECT course_grades.session_user_id
-                        , course_grades.normal_grade
-                        , lecture_submissions.lectures_viewed
-                   FROM   course_grades
-                          LEFT JOIN (
-                               SELECT session_user_id
-                                     , 100*COUNT(DISTINCT(item_id))/%s AS lectures_viewed
-                               FROM   lecture_submission_metadata
-                               GROUP BY session_user_id) lecture_submissions
-                          ON course_grades.session_user_id=lecture_submissions.session_user_id
-                   WHERE course_grades.normal_grade > 0;""" % lecture_total
-        lectures_viewed = pd.io.sql.read_frame(query, schemaconn.raw_connection())
-
-        grades_by_lectures_viewed = []
-        bin_width = 10
-        bin_start = 0
-        bin_end = 0
-        labels = []
-        while bin_end < 100:
-            bin_start = bin_end
-            bin_end += bin_width
-            if bin_end < 100:
-                grades_by_lectures_viewed.append( lectures_viewed.loc[(lectures_viewed['lectures_viewed']>=bin_start) & (lectures_viewed['lectures_viewed']<bin_end),'normal_grade'].values )
-                labels.append(str(bin_start)+'-'+str(bin_end)+'%')
-            else:
-                grades_by_lectures_viewed.append( lectures_viewed.loc[(lectures_viewed['lectures_viewed']>=bin_start) & (lectures_viewed['lectures_viewed']<=bin_end),'normal_grade'].values )
-                labels.append(str(bin_start)+'-'+str(bin_end)+'%')
-
-       # Plot course grade distribution, colored by percent of lectures viewed
-        colors = []
-        bin_num = 100/bin_width
-        for i in range(bin_num):
-            colors.append((float(i)/bin_num,0.2,1-float(i)/bin_num))
-        plt.figure()
-        n, bins, patches = plt.hist(grades_by_lectures_viewed, 100, histtype='bar', stacked=True, color=colors, label=labels)
-        plt.axvline(x=accomplishment_grade, color='#999999')
-        plt.legend(title='% of lectures accessed')
-        plt.title('Distribution of Course Grades ('+str(schema_name)+')')
-        plt.xlabel('Grade')
-        plt.ylabel('Count')
-
         query = """SELECT session_user_id
                         , normal_grade
                         , achievement_level
@@ -201,25 +265,7 @@ for schema_name in schemas:
         print '  mean='+str(mean)+', sd='+str(std)
         print '  Kolmogorov-Smirnov: D='+str(d)+', p='+str(p)
         print ''
-
-        '''
-        # Plot course grade distribution, colored by percent of lectures viewed
-        colors = []
-        labels = []
-        time_len = len(grades_by_time)
-        for i in range(time_len):
-            colors.append((float(i)/time_len,0.2,1-float(i)/time_len))
-            labels.append('Week '+str(i+1))
-        labels[time_len-1] = 'Beyond'
-        plt.figure()
-        n, bins, patches = plt.hist(grades_by_time, 100, histtype='bar', stacked=True, color=colors, label=labels)
-        plt.axvline(x=accomplishment_grade, color='#999999')
-        plt.legend(title='Last lecture view')
-        plt.title('Distribution of Course Grades')
-        plt.xlabel('Grade')
-        plt.ylabel('Count')
-        '''
     except Exception, e:
-        pass
+        print e
 
     plt.show()
