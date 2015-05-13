157ead59215959866d32d0436d69143296605c95
Brooks, Christopher <brooksch@umich.edu>
2015-05-13 17:08:48 -0400

    Changes for purva in config.py

--- util/config.py
+++ util/config.py
@@ -21,7 +21,8 @@ from sqlalchemy import *
 import logging
 import os
 from ctypes import c_bool
-
+import csv
+import io
 
 def get_properties():
     """Returns the list of properties as a dict of key/value pairs in the file config.properties."""
@@ -50,12 +51,12 @@ def get_coursera_schema_list():
     if ("schemas" in config.keys()):
         return config["schemas"].split(",")
 
-    query = "SELECT id FROM coursera_index WHERE start IS NOT NULL;"
+    query = "SELECT session_id FROM coursera_index WHERE start IS NOT NULL;"
     conn = get_connection()
     results = conn.execute(query)
     schemas = []
     for row in results:
-        schemas.append(row["id"].encode('ascii', 'ignore'))
+        schemas.append(row["session_id"])
     return schemas
 
 
@@ -63,7 +64,7 @@ def convert_sessionid_to_id(session_id):
     """Converts a session id in the form of 'introfinance-001' to a primary key such as 2.  Simple wrapper to select
     from the uselab_mooc.coursera_index table"""
     conn = get_connection()
-    rs = conn.execute("SELECT id FROM coursera_index WHERE session_id LIKE '{}'".format(session_id))
+    rs = conn.execute("SELECT admin_id FROM coursera_index WHERE session_id LIKE '{}'".format(session_id))
     id = int(rs.fetchone()[0])
     return id
 
@@ -83,6 +84,96 @@ def filename_to_schema(fn):
     return os.path.basename(fn).split(".")[0]
 
 
+class ThreadedCSVQueue(multiprocessing.Process):
+    """
+    Manages a queue for a given file so that it can be loaded later with MySQL's LOAD INFILE
+    """
+    def __init__(self, queue, connection, table, batch_size=1, log_to_console=False, hard_exit_on_failure=True,
+                 write_header=False):
+        """
+        queue should be of type Queue.Queue(), connection of type file.
+        """
+        multiprocessing.Process.__init__(self)
+        self.queue = queue
+        self.connection = connection
+        self.table = table
+        self.close = multiprocessing.Value(c_bool, False)
+        self.batch_size = batch_size
+        self.log_to_console = log_to_console
+        self.hard_exit_on_failure = hard_exit_on_failure
+        self.failures = []
+        self.file=connection
+
+        # start csv file up
+        self.columns=[]
+        for col in table.columns:
+            self.columns.append(str(col).replace("{}.".format(table.name),"")) # file is in colname, metadata in table.colname
+
+        if write_header:
+            self.writer = csv.DictWriter(connection, fieldnames=self.columns, delimiter='\t', doublequote=True, escapechar='\\',
+                                     skipinitialspace=True)
+            self.writer.writeheader()
+
+    def stop(self):
+        """
+        Indicates that the ThreadedCSVQueue should shutdown after it has emptied its queue.
+        """
+        self.close.value = True
+
+    def __insert_values(self, items):
+        """
+        Tries to append items into file.  Returns True if success else False, and exits the thread if
+        hard_exit_on_failure is set.
+        """
+        try:
+            for item in items:
+                out_string=io.StringIO('')
+                wrtr = csv.DictWriter(out_string, fieldnames=self.columns, delimiter='\t', doublequote=True, escapechar='\\', skipinitialspace=True)
+                #it seems lame to have to do this, why can't this be done autmatically by the writer?
+                for k in item.keys():
+                    if item[k]==None:
+                        item[k]='NULL'
+                missing=set(self.columns)-set(item.keys())
+                for i in missing:
+                    item[i]='NULL'
+                wrtr.writerow(item)
+
+                self.file.write(out_string.getvalue().replace("NULL",r'\N')) #supposedly this instructs mysql to insert a null
+            return True
+        except Exception as e:
+            print(e)
+            if self.batch_size>1:
+                print("There was an exception in batch insert, will report on individual values.")
+            else:
+                if self.log_to_console:
+                    print("Exception {}".format(e))
+                    print("Item: {}".format(items))
+                if self.hard_exit_on_failure:
+                    os._exit(-1)
+            return False
+
+    def run(self):
+        """
+        Start the ThreadedCSVQueue running, inserting up to batch_size items at once.
+        """
+        if self.log_to_console:
+            print("Starting CSV Queue.")
+        while self.queue.qsize() > 0 or self.close.value is False:
+            items = []
+            for item_num in range(0, self.batch_size):
+                try:
+                    items.append(self.queue.get_nowait())
+                except queue.Empty:
+                    continue
+            if self.log_to_console:
+                print("{} items being put in db for table {}, {} remain in queue.".format(len(items), self.table,
+                                                                                          self.queue.qsize()))
+            # try and add all the items in the queue and, if the batch fails, then try and add them one by one
+            if not self.__insert_values(items):
+                for item in items:
+                    if not self.__insert_values(item):
+                        self.failures.append(item)
+
 class ThreadedDBQueue(multiprocessing.Process):
     """
     Manages a queue for a given db connection, and table in separate thread.
@@ -93,7 +184,7 @@ class ThreadedDBQueue(multiprocessing.Process):
         queue should be of type Queue.Queue(), connection of type util.config.get_connection() (an SQLAlchemy engine),
         and table should be an SQLAlchemy table metadata object (e.g. metadata.tables["coursera_clickstream"]).
         """
-        multiprocessing.Process.__init__(self) # , daemon=True)
+        multiprocessing.Process.__init__(self)
         self.queue = queue
         self.connection = connection
         self.table = table
