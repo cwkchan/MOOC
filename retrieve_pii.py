cda7a580e0adc0f8332631e0d989362fe6aebe06
Purva Kulkarni <purvak@umich.edu>
2015-06-10 12:34:52 -0500

    File location changed

--- retrieve_pii.py
+++ retrieve_pii.py
@@ -55,6 +55,7 @@ opener=build_coursera_opener(browser)
 Session = sessionmaker(bind=conn)
 session = Session()
 pii = pandas.DataFrame()
+
 for course in session.query(Course):
     try:
         print("Working with {}".format(course.session_id))
@@ -64,4 +65,4 @@ for course in session.query(Course):
     except Exception as e:
         print("Received an error {} for URL: {}".format(e,'https://class.coursera.org/{}/data/export/pii_download'.format(course.session_id)))
 
-pii.to_csv("pii.csv",index=False)
+pii.to_csv("{}/pii.csv".format(get_properties()['pii']),index=False)
