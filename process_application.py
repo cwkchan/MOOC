f7bca3bf8809a5f1ae4d7f753ab0e4693690a4cf
Brooks, Christopher <brooksch@umich.edu>
2015-11-27 19:49:03 -0500

    Updated from purva's copy, hard coded in words to search for

--- process_application.py
+++ process_application.py
@@ -19,32 +19,39 @@ import re
 import PyPDF2
 import argparse
 from os import listdir
+import glob
 
 parser = argparse.ArgumentParser(description='To check the pdf applications for key words')
 parser.add_argument('--verbose', action='store_true', help='Whether to debug log or not')
+parser.add_argument('--words', help='Comma separated list of words to search for')
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('--pdfs', help='A list of pdf files to scan')
 group.add_argument('--dir', help='A directory with pdf files in it')
 args = parser.parse_args()
 
 if args.dir is not None:
-    pdfs = listdir(args.dir)
-    pdfs = map(lambda x: args.dir + x, pdfs)
+    pdfs = glob.glob(args.dir) #listdir(args.dir)
+    #pdfs = map(lambda x: args.dir + x, pdfs)
 else:
     pdfs = args.pdfs.split(',')
 
 def pdf_has_match(pdf, test_words): # tests to see if any word or phrase in test_words is in pdf
+    matches=[]
+    page_num=0
     for word in test_words:
         for page in pdf.pages:
-            line = bytes(page.extractText(), encoding="UTF-8")
-            if re.search(word, line):
-                return True
-    return False
+            if re.search(word, page.extractText()):
+                matches.append([page_num,word])
+    return matches
 
+#hard coding this for now so we can tell when reporting results
+args.words="MOOC,mooc,open online,online course,oer,OER,coursera,Coursera,edx,Edx,EDx,EDX,eDX,edX,COURSERA,udacity,udemy,course online"
 
-test_words = (get_properties()['search_phrases']) #program searches the pdf for these phrases
 for pdf_file in pdfs:
-    pdf = PyPDF2.PdfFileReader(open(pdf_file, "rb")) #open the PDF
-    if pdf_has_match(pdf, test_words):
-        print("Matched file: {}".format(pdf_file))
-
+    try:
+        pdf = PyPDF2.PdfFileReader(open(pdf_file, "rb")) #open the PDF
+        matches = pdf_has_match(pdf, args.words.split(","))
+        if len(matches)>0:
+            print("Match found in file: {}".format(pdf_file))
+    except Exception:
+        pass
