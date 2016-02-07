f7fc04605f9d581ae03194018f61dec1dfb85c0a
Josh Gardner <joshua.patrick.gardner@gmail.com>
2016-02-07 12:47:32 -0500

    Updates to commenting, spacing, and credentialing

--- send_email_alert.py
+++ send_email_alert.py
@@ -1,53 +1,33 @@
 
 import boto3 
-#note: boto3 is the newest version of boto and is distinct from previous boto module.
+from util.config import *
 
-
-
-
-
-def send_email_alert(sender, subject, body, to, aws_access_key_id, aws_secret_access_key, format="text"):
+def send_email_alert(sender, subject, body, to, format="text"):
     """
-    If you're using this with new credentials or change the sender, you may need to verify the sender address.
-
-    To verify a new email address, use the following command (replace with desired sender address):
-
-    botoconn.verify_email_address('led-alerts@umich.edu')
-
-    You will need to click the link in the verification email sent to this address to be able to send email from it.
+    Sends email alert.
+    You may need to verify the sender address by using botoconn.verify_email_address('led-alerts@umich.edu') and clicking the link in the verification email sent to this address.
     """
     
+    aws_access_key = get_properties().get('access_id', None)
+    aws_secret_key = get_properties().get('secret_key', None)
 
     client = boto3.client('ses', region_name='us-east-1', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
     alert_destination = {
-
-    'ToAddresses': [
-            to,
-        ],
-        'CcAddresses': [
-            'joshua.patrick.gardner@gmail.com',
-        ]
-    }
-
-    #import ipdb; ipdb.set_trace()
+        'ToAddresses': [to,],
+        'CcAddresses': [None,]
+        }
 
     if format=="html":
         alert_contents = {
-
         'Subject' : {'Data' : subject},
-
-        'Body' : {'Html' : { 'Data' : body}
-             }
+        'Body' : {'Html' : { 'Data' : body}}
         }
     
     else:
         alert_contents = {
-
         'Subject' : {'Data' : subject},
-
-        'Body' : {'Text' : { 'Data' : body}
-             }
+        'Body' : {'Text' : { 'Data' : body}}
         }
 
     client.send_email(Source = sender, Destination = alert_destination, Message = alert_contents)
